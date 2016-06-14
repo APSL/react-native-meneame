2b6d189b6a38fac2c1a87b8a76e91a7bfa2b3ed3
Daniel Sastre <dsastre@apsl.net>
2016-06-14 15:24:22 +0200

    Android drawer header and full app name

--- app/Meneame.js
+++ app/Meneame.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet, Image, Text, Dimensions } from 'react-native'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import Ionicon from 'react-native-vector-icons/Ionicons'
 import { Menu } from 'react-native-section-menu'
@@ -12,14 +13,46 @@ import {
 } from './MnmSections'
 
 
+const MnmAndroidHeader = (props) =>
+  <Image source={require('./img/header.png')} style={styles.headerContainer} resizeMode="cover">
+    <Text style={styles.headerText}>Menéame</Text>
+    <Text style={styles.versionText}>Versión 1.0</Text>
+  </Image>
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    width: Dimensions.get('window').width - 56,
+    height: ((Dimensions.get('window').width - 56) * 9) / 16,
+    backgroundColor: 'coral',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 24,
+    marginLeft: 14,
+    marginBottom: 4,
+  },
+  versionText : {
+    color: 'white',
+    fontSize: 14,
+    marginLeft: 14,
+    marginBottom: 8,
+  }
+})
+
+
 class Meneame extends React.Component {
   render () {
     const androidIconSize = 18
     const iOSiconSize = 30
     let itemId = 0
+    const header = <MnmAndroidHeader />
 
     return (
       <Menu barTintColor="black" tintColor="#d35400" initialEntry={0}
+        header={header}
         entries={[
           {
             id: itemId++,
