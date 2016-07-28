90ca709a3081204113d917022db235d14c315ba4
alvaro <me@alvaromb.com>
2016-07-28 09:58:22 +0200

    closes #6 Added pull to refresh to news list and comments

--- app/MnmComments.js
+++ app/MnmComments.js
@@ -1,94 +1,109 @@
 /*jshint esnext: true*/
 /*global require, module, fetch*/
 
-'use strict';
-
-import React, { Component } from 'react'
+import React from 'react'
 import {
-    StyleSheet,
-    Platform,
-    PixelRatio,
-    View,
-    Text,
-    ActivityIndicatorIOS,
-    ProgressBarAndroid,
-    ListView
+  StyleSheet,
+  Platform,
+  PixelRatio,
+  View,
+  Text,
+  ActivityIndicatorIOS,
+  ProgressBarAndroid,
+  ListView,
+  RefreshControl,
+  InteractionManager
 } from 'react-native'
-
 import HTMLView from 'react-native-htmlview'
+var moment = require('moment')
+var Icon = require('react-native-vector-icons/EvilIcons')
 
-var moment = require('moment');
-var Icon = require('react-native-vector-icons/EvilIcons');
+class MnmComments extends React.Component {
+  getComments: Function;
 
-
-class MnmComments extends Component {
-    constructor(props) {
-      super(props);
-      var dataSource = new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1.id !== r2.id
-      });
-      this.state = {
-        dataSource: dataSource.cloneWithRows([]),
-        rows: []
-      };
-      this._getComments();
+  constructor (props) {
+    super(props)
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.id !== r2.id
+    })
+    this.state = {
+      dataSource: dataSource.cloneWithRows([]),
+      rows: [],
+      isFetching: false,
     }
+    this.getComments = this._getComments.bind(this)
+  }
 
-    _getComments() {
-        fetch('https://www.meneame.net/api/list?id=' + this.props.entryId)
-        .then(response => response.json())
-        .then(response => {
-            var comments = [];
-            response.objects.forEach((comment) => {
-                comment.date = moment.unix(comment.date);
-                comment.fromNow = comment.date.fromNow();
-                comments.push(comment);
-            });
-            var sortedComments = comments.sort((c1, c2) => {
-                if (c1.order < c2.order) {
-                    return -1;
-                } else {
-                    return 1;
-                }
-            });
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(sortedComments),
-                rows: sortedComments
-            });
-        });
-    }
+  componentDidMount () {
+    this.getComments()
+  }
 
-    renderRow(rowData) {
-      return (
-        <View style={styles.cellContainer}>
-          <View style={styles.infoContainer}>
-            <Text style={styles.username}>{rowData.user}</Text>
-            <View style={styles.commentData}>
-              <Icon style={styles.icon} name="like" size={20} />
-              <Text style={styles.counter}>{rowData.votes}</Text>
-              <Icon style={styles.icon} name="heart" size={20} />
-              <Text style={styles.counter}>{rowData.karma}</Text>
-              <Text style={styles.date}>{rowData.fromNow}</Text>
-            </View>
-          </View>
-          <View style={styles.textContainer}>
-            <Text style={styles.commentNumber}>#{rowData.order}</Text>
-            <Text style={styles.comment}>
-              <HTMLView value={rowData.content} stylesheet={htmlStyles} />
-            </Text>
+  _getComments () {
+    InteractionManager.runAfterInteractions(() => {
+      this.setState({isFetching: true})
+      fetch('https://www.meneame.net/api/list?id=' + this.props.entryId)
+      .then(response => response.json())
+      .then(response => {
+        const comments = response.objects.map((comment) => {
+          comment.date = moment.unix(comment.date)
+          comment.fromNow = comment.date.fromNow()
+          return comment
+        })
+        .sort((c1, c2) => {
+          if (c1.order < c2.order) {
+            return -1;
+          } else {
+            return 1;
+          }
+        })
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(comments),
+          rows: comments,
+          isFetching: false,
+        })
+      })
+      .catch(() => this.setState({isFetching: false}))
+    })
+  }
+
+  renderRow(rowData) {
+    return (
+      <View style={styles.cellContainer}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.username}>{rowData.user}</Text>
+          <View style={styles.commentData}>
+            <Icon style={styles.icon} name="like" size={20} />
+            <Text style={styles.counter}>{rowData.votes}</Text>
+            <Icon style={styles.icon} name="heart" size={20} />
+            <Text style={styles.counter}>{rowData.karma}</Text>
+            <Text style={styles.date}>{rowData.fromNow}</Text>
           </View>
         </View>
-      );
-    }
+        <View style={styles.textContainer}>
+          <Text style={styles.commentNumber}>#{rowData.order}</Text>
+          <Text style={styles.comment}>
+            <HTMLView value={rowData.content} stylesheet={htmlStyles} />
+          </Text>
+        </View>
+      </View>
+    )
+  }
 
-  _renderList() {
+  _renderList () {
     if (this.state.rows.length > 0) {
       return (
-        <ListView style={styles.navcomments}
+        <ListView
+          style={styles.navcomments}
           contentInset={{bottom: 49}}
           dataSource={this.state.dataSource}
           renderRow={this.renderRow.bind(this)}
           automaticallyAdjustContentInsets={false}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isFetching}
+              onRefresh={this.getComments}
+            />
+          }
         />
       )
     } else {
@@ -109,92 +124,92 @@ class MnmComments extends Component {
     }
   }
 
-  render() {
-      return (
-          <View style={styles.container}>
-              {this._renderList()}
-          </View>
-      );
+  render () {
+    return (
+      <View style={styles.container}>
+        {this._renderList()}
+      </View>
+    )
   }
 }
 
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FAFAFA',
-    },
-    navcomments: {
-        flex: 1,
-        backgroundColor: '#FAFAFA',
-    },
-    cellContainer: {
-        flex: 1,
-        backgroundColor: '#FAFAFA',
-        marginLeft: 20,
-        marginRight: 20,
-        marginBottom: 10,
-        paddingBottom: 15,
-        paddingTop: 5,
-        borderBottomWidth: 1 / PixelRatio.get(),
-        borderBottomColor: '#CDCDCD',
-    },
-    infoContainer: {
-        flexDirection: 'row',
-        flex: 1,
-        alignItems: 'stretch',
-        marginBottom: 10,
-    },
-    username: {
-        flex: 1,
-        flexDirection: 'row',
-        fontWeight: 'bold',
-        fontSize: 14,
-        color: '#262626',
-    },
-    commentData: {
-        flex: 2,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-    },
-    icon: {
-        width: 20,
-        color: '#95a5a6',
-    },
-    counter: {
-        color: '#95a5a6',
-        marginRight: 10,
-    },
-    date: {
-        fontSize: 14,
-        fontWeight: '300',
-        color: '#95a5a6',
-    },
-    textContainer: {
-        flexDirection: 'row',
-    },
-    commentNumber: {
-        color: '#d35400',
-        fontWeight: '300',
-        fontSize: 14,
-    },
-    comment: {
-        marginLeft: 3,
-        flex: 1,
-        color: '#7f8c8d',
-        fontWeight: '300',
-        fontSize: 14,
-    },
-    centering: {
-        flex: 1,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#FAFAFA',
-    },
-    progressBar: {
-      width: 50,
-      height: 50,
-    },
-});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+  },
+  navcomments: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+  },
+  cellContainer: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
+    paddingBottom: 15,
+    paddingTop: 5,
+    borderBottomWidth: 1 / PixelRatio.get(),
+    borderBottomColor: '#CDCDCD',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'stretch',
+    marginBottom: 10,
+  },
+  username: {
+    flex: 1,
+    flexDirection: 'row',
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#262626',
+  },
+  commentData: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  icon: {
+    width: 20,
+    color: '#95a5a6',
+  },
+  counter: {
+    color: '#95a5a6',
+    marginRight: 10,
+  },
+  date: {
+    fontSize: 14,
+    fontWeight: '300',
+    color: '#95a5a6',
+  },
+  textContainer: {
+    flexDirection: 'row',
+  },
+  commentNumber: {
+    color: '#d35400',
+    fontWeight: '300',
+    fontSize: 14,
+  },
+  comment: {
+    marginLeft: 3,
+    flex: 1,
+    color: '#7f8c8d',
+    fontWeight: '300',
+    fontSize: 14,
+  },
+  centering: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FAFAFA',
+  },
+  progressBar: {
+    width: 50,
+    height: 50,
+  },
+})
 
 const htmlStyles = StyleSheet.create({
   b: {
@@ -202,4 +217,4 @@ const htmlStyles = StyleSheet.create({
   }
 })
 
-module.exports = MnmComments;
+module.exports = MnmComments
--- app/MnmEntrada.js
+++ app/MnmEntrada.js
@@ -1,124 +1,123 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
-    StyleSheet,
-    View,
-    Text,
-    Navigator,
-    TouchableHighlight
+  StyleSheet,
+  View,
+  Text,
+  Navigator,
+  TouchableHighlight,
+  Dimensions
 } from 'react-native'
-
-var screen = require('Dimensions').get('window');
-var Button = require('react-native-button');
-var ParallaxView = require('react-native-parallax-view');
-var ThumborURLBuilder = require('thumbor-url-builder');
-
+import Button from 'react-native-button'
+import ParallaxView from 'react-native-parallax-view'
+import ThumborURLBuilder from 'thumbor-url-builder'
 import { THUMBOR_KEY, THUMBOR_URL } from './ThumborConfig'
 import MnmEntryTextAndDetails from './MnmEntryTextAndDetails'
-var MnmComments = require('./MnmComments');
-var MnmWebviewEntry = require('./MnmWebviewEntry');
+import MnmComments from './MnmComments'
+import MnmWebviewEntry from './MnmWebviewEntry'
 import { BrowserButton } from './MnmRouteMapper'
 
+const screen = Dimensions.get('window')
 
-class NavButton extends Component {
-    render() {
-        return (
-            <Button style={styles.button} onPress={this.props.onPress}>
-                {this.props.text}
-            </Button>
-        );
-    }
+class NavButton extends React.Component {
+  render() {
+    return (
+      <Button style={styles.button} onPress={this.props.onPress}>
+        {this.props.text}
+      </Button>
+    )
+  }
 }
 
-class MnmEntrada extends Component {
-    constructor(props) {
-        super(props);
-        var thumborURL = new ThumborURLBuilder(THUMBOR_KEY, THUMBOR_URL);
-        if (this.props.entrada.thumb) {
-            var entry = this.props.entrada;
-            var imagePath = entry.thumb.substr(8, entry.thumb.length);
-            this.props.entrada.mediaHeader = thumborURL.setImagePath(imagePath).resize(screen.width * screen.scale, screen.height).smartCrop(true).buildUrl();
-        }
-        this.state = {
-            value: 'Noticia'
-        };
+class MnmEntrada extends React.Component {
+  constructor(props) {
+    super(props);
+    const thumborURL = new ThumborURLBuilder(THUMBOR_KEY, THUMBOR_URL);
+    if (this.props.entrada.thumb) {
+      const entry = this.props.entrada;
+      const imagePath = escape(entry.thumb.substr(8, entry.thumb.length))
+      this.props.entrada.mediaHeader = thumborURL.setImagePath(imagePath).resize(screen.width * screen.scale, screen.height).smartCrop(true).buildUrl();
     }
+    this.state = {
+      value: 'Noticia'
+    }
+  }
 
-    _titlePressed() {
-      const URL = this.props.entrada.go
-      this.props.navigator.push({
-        title: this.props.entrada.from,
-        component: MnmWebviewEntry,
-        passProps: {uri: URL},
-        rightElement: <BrowserButton url={URL} />
-      });
-    }
+  _titlePressed() {
+    const URL = this.props.entrada.go
+    this.props.navigator.push({
+      title: this.props.entrada.from,
+      component: MnmWebviewEntry,
+      passProps: {uri: URL},
+      rightElement: <BrowserButton url={URL} />
+    })
+  }
 
-    render() {
-        const entrada = this.props.entrada
-        const backgroundSource = entrada.mediaHeader ? {uri: entrada.mediaHeader} : null
-        return (
-            <ParallaxView
-                backgroundSource={backgroundSource}
-                windowHeight={250}
-                style={{backgroundColor: '#FAFAFA'}}
-                contentInset={{bottom: 49}}>
-                <View style={styles.container}>
-                    <View style={styles.info}>
-                        <TouchableHighlight
-                            onPress={this._titlePressed.bind(this)}
-                            underlayColor={'#FAFAFA'}>
-                            <Text style={styles.title}>{entrada.title}</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.from}>{entrada.from}</Text>
-                    </View>
-                    <MnmEntryTextAndDetails entry={entrada} />
-                    <NavButton onPress={() => {
-                        this.props.navigator.push({
-                            title: 'Comentarios',
-                            component: MnmComments,
-                            sceneConfig: Navigator.SceneConfigs.FloatFromBottom,
-                            passProps: {entryId: entrada.id}
-                        });
-                    }} text={'Comentarios (' + entrada.comments + ')'}
-                       style={styles.button}/>
-                </View>
-            </ParallaxView>
-        );
-    }
+  render() {
+    const entrada = this.props.entrada
+    const backgroundSource = entrada.mediaHeader ? {uri: entrada.mediaHeader} : null
+    return (
+      <ParallaxView
+        backgroundSource={backgroundSource}
+        windowHeight={250}
+        style={{backgroundColor: '#FAFAFA'}}
+        contentInset={{bottom: 49}}>
+        <View style={styles.container}>
+          <View style={styles.info}>
+            <TouchableHighlight
+              onPress={this._titlePressed.bind(this)}
+              underlayColor={'#FAFAFA'}>
+              <Text style={styles.title}>{entrada.title}</Text>
+            </TouchableHighlight>
+            <Text style={styles.from}>{entrada.from}</Text>
+          </View>
+          <MnmEntryTextAndDetails entry={entrada} />
+          <NavButton onPress={() => {
+              this.props.navigator.push({
+                title: 'Comentarios',
+                component: MnmComments,
+                sceneConfig: Navigator.SceneConfigs.FloatFromBottom,
+                passProps: {entryId: entrada.id}
+              })
+            }} text={'Comentarios (' + entrada.comments + ')'}
+            style={styles.button}/>
+        </View>
+      </ParallaxView>
+    )
+  }
 }
 
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FAFAFA',
-    },
-    info: {
-      marginLeft: 20,
-      marginRight: 20,
-      marginTop: 10,
-      marginBottom: 10,
-      paddingBottom: 10,
-    },
-    title: {
-        fontWeight: '300',
-        color: '#262626',
-        fontSize: 20,
-    },
-    from: {
-        fontWeight: '300',
-        color: '#7f8c8d',
-        fontSize: 12,
-        marginBottom: 5,
-    },
-    button: {
-        color: '#d35400',
-        marginTop: 10,
-        fontSize: 22,
-        paddingTop: 15,
-        paddingBottom: 25,
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+  },
+  info: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingBottom: 10,
+  },
+  title: {
+    fontWeight: '300',
+    color: '#262626',
+    fontSize: 20,
+  },
+  from: {
+    fontWeight: '300',
+    color: '#7f8c8d',
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  button: {
+    color: '#d35400',
+    marginTop: 10,
+    fontSize: 22,
+    paddingTop: 15,
+    paddingBottom: 25,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+})
 
-module.exports = MnmEntrada;
+module.exports = MnmEntrada
